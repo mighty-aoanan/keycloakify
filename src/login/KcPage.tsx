c0f05929ca87d6b5d408e32c68fdb18eba29b376
mighty.aoanan <mighty.aoanan@metrobank.com.ph>
2025-11-26 12:15:55 +0800

    initia commit

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -1,9 +1,10 @@
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import type { ClassKey } from "keycloakify/login";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
-import Template from "keycloakify/login/Template";
+import Template from "./Template";
+import "./main.css";
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
@@ -14,6 +15,14 @@ export default function KcPage(props: { kcContext: KcContext }) {
     const { kcContext } = props;
 
     const { i18n } = useI18n({ kcContext });
+
+    useEffect(() => {
+        switch (kcContext.themeName) {
+            case "defaultMBO":
+                import("./mainMBO.css");
+                break;
+        }
+    }, []);
 
     return (
         <Suspense>
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     plugins: [
         react(),
         keycloakify({
+            themeName: ["default", "defaultMBO"],
             accountThemeImplementation: "none"
         })
     ]
